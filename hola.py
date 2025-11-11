809dd3872a2cc793c1021941c599d48be1378f42
Wilson Sabogal <wilsonandres4@hotmail.com>
2025-11-10 20:32:21 -0500

    Mejoras a la calculadora

--- hola.py
+++
@@ -1,6 +0,0 @@
-# hola.py
-# Autor: Wilson Sabogal
-# Descripción: Script básico que saluda al usuario
-nombre = input("¿Como te llamas? ")
-print(f"¡Hola, {nombre}! Bienvenido a mi primer proyecto con Git y Python. Te habla Wilson Sabogal")
-print(f"Tu nombre tiene {len(nombre)} letras.")
