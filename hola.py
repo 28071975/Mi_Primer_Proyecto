2190a9dda8229aa9b63ef68a3b259c04fa46bf1c
Wilson Sabogal <wilsonandres4@hotmail.com>
2025-11-10 20:04:53 -0500

    Segunda versión: se muestra la longitud del nombre

--- hola.py
+++ hola.py
@@ -3,3 +3,4 @@
 # Descripción: Script básico que saluda al usuario
 nombre = input("¿Como te llamas? ")
 print(f"¡Hola, {nombre}! Bienvenido a mi primer proyecto con Git y Python. Te habla Wilson Sabogal")
+print(f"Tu nombre tiene {len(nombre)} letras.")
