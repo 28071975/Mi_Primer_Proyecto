13404f146a2cdeb292861977887beb2fd8843dbf
Wilson Sabogal <wilsonandres4@hotmail.com>
2025-11-19 23:16:14 -0500

    Nueva subida y para ver si la rama funciona

--- hola.py
+++ hola.py
@@ -4,3 +4,5 @@
 nombre = input("¿Como te llamas? ")
 print(f"¡Hola, {nombre}! Bienvenido a mi primer proyecto con Git y Python. Te habla Wilson Sabogal")
 print(f"Tu nombre tiene {len(nombre)} letras.")
+# Edición de la rama
+# Nueva rama para ver si todo lo que estoy brancheando se sube bien
